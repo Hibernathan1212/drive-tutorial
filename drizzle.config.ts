809a9b07cb860b53e6f26c13ef710406b473e239
Nathan <nnthurber@icloud.com>
2025-04-20 17:46:37 +0700

    added database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,66 @@
+// import { type Config } from "drizzle-kit";
+
+// import { env } from "~/env";
+
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   dialect: "sqlite",
+//   dbCredentials: {
+//     url: env.DATABASE_URL,
+//   },
+//   tablesFilter: ["drive-tutorial_*"],
+// } satisfies Config;
+
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   dialect: "singlestore",
+//   tablesFilter: ["drive-tutorial_*"],
+// } satisfies Config;
+
+// import { defineConfig } from "drizzle-kit";
+
+// export default defineConfig({
+//   schema: "./src/server/db/schema.ts",
+//   dialect: "singlestore",
+//   tablesFilter: ["drive-tutorial_*"],
+//   dbCredentials: {
+//     host: env.SINGLESTORE_HOST,
+//     port: parseInt(env.SINGLESTORE_PORT),
+//     user: env.SINGLESTORE_USER,
+//     password: env.SINGLESTORE_PASS,
+//     database: env.SINGLESTORE_DB_NAME,
+//     ssl: {},
+//   },
+// })
+
+// import { defineConfig } from "drizzle-kit";
+// export default defineConfig({
+//     dialect: "singlestore",
+//     schema: "<path/to/schema>",
+//     dbCredentials: {
+//         host: "svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com",
+//         user: "nathan newton-ee17a",
+//         password: "<nathan newton-ee17a Password>",
+//         port: 3333,
+// 	database: "DRIVE_NATHAN_DB",
+// 	ssl: {},
+//     },
+// });
+
 import { type Config } from "drizzle-kit";
 
 import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
+  tablesFilter: ["drive_tutorial_*"],
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
-  tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/singlestore";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -9,11 +9,33 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  conn: Pool | undefined;
 };
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const conn = 
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn=conn;
 
-export const db = drizzle(client, { schema });
+conn.addListener("error", (err) => {
+  console.error("Database connection error:", err);
+})
+
+// export const client =
+//   globalForDb.client ?? createClient({ url: env.DATABASE_URL });
+// if (env.NODE_ENV !== "production") globalForDb.client = client;
+
+// export const db = drizzle(client, { schema });
+
+// import mysql from "mysql2/promise";
+// import { drizzle } from "drizzle-orm/singlestore";
+// const pool = mysql.createPool("singlestore://nathan newton-ee17a:<nathan newton-ee17a Password>@svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com:3333/DRIVE_NATHAN_DB?ssl={}");
+const db = drizzle({ client: conn });
