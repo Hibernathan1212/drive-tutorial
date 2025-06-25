daf1e2f0db6ece536d4707ec57ac584d553350e3
Nathan <nnthurber@icloud.com>
2025-06-25 15:41:43 +0700

    cleaned up queries

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,23 +1,6 @@
-import { db } from "~/server/db";
-import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema";
 import DriveContents from "../../drive-contents";
-import { eq } from "drizzle-orm";
+import { getFiles, getFolders, getAllParentsForFolder } from "~/server/db/queries";
 
-async function getAllParents(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
-  while (currentId !== null) {
-    const folder = await db.selectDistinct().from(foldersSchema).where(eq(foldersSchema.id, currentId));
-
-    if (!folder[0]) {
-      throw new Error("Parent folder not found");
-    }
-
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent ?? null;
-  }
-  return parents;
-}
 
 export default async function GoogleDriveClone(props: {
     params: Promise<{ folderId: string }>;
@@ -31,9 +14,9 @@ export default async function GoogleDriveClone(props: {
 
   console.log(parsedFolderId);
 
-  const filesPromise = db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
-  const foldersPromise = db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
-  const parentsPromise = getAllParents(parsedFolderId);
+  const filesPromise = getFiles(parsedFolderId);
+  const foldersPromise = getFolders(parsedFolderId);
+  const parentsPromise = getAllParentsForFolder(parsedFolderId);
 
   const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise]);
 
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -6,7 +6,7 @@ export const createTable = singlestoreTableCreator(
   (name) => `drive-tutorial_${name}`,
 );
 
-export const files = createTable(
+export const files_table = createTable(
   "files_table", 
   {
     id: bigint("id", {mode: "number", unsigned: true }).primaryKey().autoincrement(),
@@ -20,7 +20,7 @@ export const files = createTable(
   },
 );
 
-export const folders = createTable(
+export const folders_table = createTable(
   "folders_table", 
   {
     id: bigint("id", {mode: "number", unsigned: true }).primaryKey().autoincrement(),
