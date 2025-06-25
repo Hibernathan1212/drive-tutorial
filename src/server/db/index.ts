aac56d532c79695fadfe271e32cc8bbed0b9586b
Nathan <nnthurber@icloud.com>
2025-06-25 12:50:46 +0700

    Completed db model

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -38,4 +38,4 @@ conn.addListener("error", (err) => {
 // import mysql from "mysql2/promise";
 // import { drizzle } from "drizzle-orm/singlestore";
 // const pool = mysql.createPool("singlestore://nathan newton-ee17a:<nathan newton-ee17a Password>@svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com:3333/DRIVE_NATHAN_DB?ssl={}");
-const db = drizzle({ client: conn });
+export const db = drizzle({ client: conn });
